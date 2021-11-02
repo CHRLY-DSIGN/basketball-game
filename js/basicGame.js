ccedcb32c49e297118cba5986b957f347ea4c127
RaselJam <Rasel.jam.h@gmail.com>
2021-11-02 11:54:20 +0100

    Added visual perspective

--- js/basicGame.js
+++ js/basicGame.js
@@ -1,20 +1,18 @@
 let T = 0
-let radios = 63
 let oldTimestamp = 0
 let secondsPassed = 0
 const animationSpeed = 40
 let angle = 0
-let X0 = window.innerWidth 
+let X0 = window.innerWidth
 let Y0 = window.innerHeight
 let y = Y0
 let x = X0
+const initialBallSize = X0 / 13.5
+let radios = initialBallSize
 let speed = 0
 let hipo = 0
 let timeElapsed = 0
 const g = 0.3
-
-
-
 
 const basicGame = {
 	ctx: undefined,
@@ -26,12 +24,9 @@ const basicGame = {
 	points: 0,
 	player: undefined,
 	shooterLogic: undefined,
-	
-	
-	
-	
+
 	init() {
-		console.log('funciono')
+		//console.log('funciono')
 		this.setContext()
 		this.setDimensions()
 		this.createBackground()
@@ -41,16 +36,10 @@ const basicGame = {
 		this.start()
 	},
 
-
-
-
 	setContext() {
 		this.canvasDOM = document.querySelector('#myCanvas')
 		this.ctx = this.canvasDOM.getContext('2d')
 	},
-
-
-
 
 	setDimensions() {
 		this.canvasSize.width = window.innerWidth
@@ -60,15 +49,12 @@ const basicGame = {
 		this.canvasDOM.setAttribute('height', this.canvasSize.height)
 	},
 
-
-
-
 	start() {
 		this.intervalId = setInterval(() => {
 			this.framesCounter++
 			this.clearScreen()
 			this.drawElements()
-			console.log('T is :', T)
+			// console.log('T is :', T)
 			if (T !== 0) {
 				this.update()
 			}
@@ -76,61 +62,50 @@ const basicGame = {
 		this.framesCounter > 50 ? clearInterval(this.intervalId) : null
 	},
 
-
-
-
 	update() {
-		console.log('Updating ...')
 		T += 1
-		console.log('speed:', speed)
-		console.log('angle :', angle)
+		// console.log('speed:', speed)
+		// console.log('angle :', angle)
 
 		if (angle < 40) {
-			angle = angle +20
+			angle = angle + 20
 		}
 
 		x = speed * Math.cos((-angle * Math.PI) / 180) * T + X0
-		y = 0.5 * g * T ** 2 + 3 * speed * Math.sin((-angle * Math.PI) / 180) * T + Y0
-		//radios = radios * 0.994
-		if (radios > 35) {
-			radios = radios * 0.991
+		y =
+			0.5 * g * T ** 2 + 3 * speed * Math.sin((-angle * Math.PI) / 180) * T + Y0
+		let variator = 1
+		let deltaX = x - X0
+		variator = variator + deltaX / 500
+		console.log('DeltaX :', deltaX)
+		if (variator <= 2) {
+			console.log('variator:', variator)
+			radios = initialBallSize / variator
 		}
-		
-		console.log('printing Circle cordination : ', x, y)
 	},
 
-
-
 	shoot(res) {
-		console.log('Shooted')
+		//	console.log('Shooted')
 		T++
 		hipo = res.hipo
 		timeElapsed = res.timeElapsed
 		angle = res.angle
 		speed = hipo / timeElapsed
-		console.log(
-			'result from shooter shootEvent in shoot() and asigne in global variable :',
-			hipo,
-			angle,
-			timeElapsed
-		)
+		// console.log(
+		// 	'result from shooter shootEvent in shoot() and asigne in global variable :',
+		// 	hipo,
+		// 	angle,
+		// 	timeElapsed
+		// )
 	},
-
-
 
 	clearScreen() {
 		this.ctx.clearRect(0, 0, this.canvasSize.width, this.canvasSize.height)
 	},
 
-
-
-
 	gameOver() {
 		clearInterval(this.intervalId)
 	},
-
-
-
 
 	drawElements() {
 		this.createBackground()
@@ -141,24 +116,17 @@ const basicGame = {
 		this.createBallStand()
 	},
 
-
-
 	drawBallTest() {
 		// console.log('Drawing ...X,y', x, y)
-		this.circleImg = new Image();
+		this.circleImg = new Image()
 		this.circleImg.src = '../IMAGES/basketball_game_ball.png'
-		this.ctx.drawImage(this.circleImg, x/2.15, y/3, x/13.5, x/13.5 )
-		
-		
-		
+		this.ctx.drawImage(this.circleImg, x / 2.15, y / 3, radios, radios)
+
 		/* this.ctx.globalCompositeOperation='destination-in';
 		this.ctx.beginPath()
 		this.ctx.arc(x, y, radios, 0, 2 * Math.PI)
 		this.ctx.fill() */
-		
 	},
-
-
 
 	createBackground() {
 		this.background = new Background(
@@ -171,9 +139,6 @@ const basicGame = {
 		this.background.draw()
 	},
 
-
-
-
 	createBasket() {
 		this.basket = new BasketStandar(
 			this.ctx,
@@ -184,9 +149,6 @@ const basicGame = {
 		)
 		this.basket.draw()
 	},
-
-
-
 
 	createPlayer() {
 		this.player = new Player(
@@ -199,9 +161,6 @@ const basicGame = {
 		this.player.draw()
 	},
 
-
-
-
 	createBall() {
 		/* this.ball = new Ball(this.ctx, 780, 320, this.canvasSize.width/13.54, this.canvasSize.height/7.62); */
 		this.ball = new Ball(
@@ -213,9 +172,6 @@ const basicGame = {
 		)
 		this.ball.draw()
 	},
-
-
-
 
 	createBallStand() {
 		this.ballStand = new BallStand(
